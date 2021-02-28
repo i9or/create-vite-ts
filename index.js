e5ab2205e652e1d6eb1a8062295ed217e713058c
Igor <2616427+i9or@users.noreply.github.com>
2021-02-28 20:32:20 +0100

    Add target directory selector

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 #!/usr/bin/env node
 
-console.log("Nothing to see here people, move along!");
+const fs = require('fs');
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+const { prompt } = require('enquirer');
+
+const cwd = process.cwd();
+
+const renameFiles = {
+  _gitignore: '.gitignore'
+}
 
 async function init() {
+  let targetDir = argv._[0];
 
+  if (!targetDir) {
+    const { projectName } = await prompt({
+      type: 'input',
+      name: 'projectName',
+      message: 'Enter project name:',
+      initial: 'vite-ts-project'
+    });
+    targetDir = projectName
+  }
+
+  const root = path.join(cwd, targetDir);
+  console.info(`\nProject will be created in ${root}`);
+
+  // TODO: check if target directory is empty
+
+  const templateDir = path.join(__dirname, 'template');
+  console.info(`Template: ${templateDir}`);
+  console.info(`${path.basename(root)}`)
 }
 
 init().catch(error => {
