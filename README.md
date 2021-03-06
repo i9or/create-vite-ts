b71a653d9bed8325964c85b441398da3bb3dd98e
Igor <2616427+i9or@users.noreply.github.com>
2021-03-06 21:41:43 +0100

    Finish basic template

--- README.md
+++ README.md
@@ -2,19 +2,27 @@
 
 ![npm version](https://img.shields.io/npm/v/create-vite-ts?label=&color=)
 
-Opinionated but minimalistic Vite TypeScript starter template.
+Opinionated but minimalistic Vite TypeScript starter template for vanilla TypeScript development and prototyping.
 
 ## Features
 
 * TypeScript
 * **WIP** Prettier + Husky + Pretty Quick
 * **WIP** PostCSS + Autoprefixer
+* **WIP** Canvas template
+* **WIP** WebGL template
+
+## Usage
+
+```
+npm init vite-ts <app-name>
+```
 
 ## Attribution
 
 Based
 on [@vite/create-app](https://github.com/vitejs/vite/tree/main/packages/create-app)
-by Evan You
+by Evan You.
 
 TypeScript [logo](https://github.com/microsoft/TypeScript-Website/blob/f407e1ae19e5e990d9901ac8064a32a8cc60edf0/packages/typescriptlang-org/static/branding/ts-logo-512.svg)
-by Microsoft
+by Microsoft.
--- index.js
+++ index.js
@@ -1,39 +1,88 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const argv = require('minimist')(process.argv.slice(2));
-const { prompt } = require('enquirer');
+import {fileURLToPath} from 'url';
+import path from 'path';
+import fs from 'fs';
+import {copyFile, mkdir, readdir, readFile, writeFile} from 'fs/promises';
+import yargs from 'yargs';
+import enquirer from 'enquirer';
 
-const cwd = process.cwd();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const renameFiles = {
-  _gitignore: '.gitignore'
+/**
+ * @type {{ _: Array.<string> }}
+ */
+const argv = yargs(process.argv.slice(2)).argv;
+
+const filesToRename = {
+  '_gitignore': '.gitignore'
 }
 
-async function init() {
-  let targetDir = argv._[0];
+async function copyFilesRecursively(srcDir, destDir) {
+  const directoryEntries = await readdir(srcDir, {withFileTypes: true});
+  for (const entry of directoryEntries) {
+    if (entry.name === 'package.json') {
+      continue;
+    }
 
-  if (!targetDir) {
-    const { projectName } = await prompt({
+    const isDirectory = entry.isDirectory();
+
+    if (isDirectory) {
+      const sourceDirectoryToCopy = path.join(srcDir, entry.name);
+      const destinationDirectoryCopyTo = path.join(destDir, entry.name);
+      await mkdir(destinationDirectoryCopyTo);
+      await copyFilesRecursively(sourceDirectoryToCopy, destinationDirectoryCopyTo);
+    } else {
+      const destFileName = filesToRename[entry.name] ? filesToRename[entry.name] : entry.name;
+      const destPath = path.join(destDir, destFileName);
+      await copyFile(path.join(srcDir, entry.name), destPath, fs.constants.COPYFILE_EXCL)
+    }
+  }
+}
+
+async function main() {
+  const cwd = process.cwd();
+
+  let projectName = argv._[0];
+  if (!projectName) {
+    const {name} = await enquirer.prompt({
       type: 'input',
-      name: 'projectName',
-      message: 'Enter project name:',
-      initial: 'vite-ts-project'
+      name: 'name',
+      message: "Enter project name:",
+      initial: "vite-ts-project"
     });
-    targetDir = projectName
+
+    projectName = name;
   }
 
-  const root = path.join(cwd, targetDir);
-  console.info(`\nProject will be created in ${root}`);
+  const targetDirectory = path.join(cwd, projectName);
+  console.info(`\nProject will be created in ${targetDirectory}`);
+  // TODO: Add check for directory existence and ask if can be removed
+  await mkdir(targetDirectory);
 
-  // TODO: check if target directory is empty
+  // TODO: Add template selector
+  const templateDir = path.join(__dirname, 'template-ts');
+  await copyFilesRecursively(templateDir, targetDirectory);
 
-  const templateDir = path.join(__dirname, 'template');
-  console.info(`Template: ${templateDir}`);
-  console.info(`${path.basename(root)}`)
+  const packageName = path.basename(targetDirectory);
+  const templatePackageJsonPath = path.join(templateDir, 'package.json');
+  const templatePackageJsonContents = await readFile(templatePackageJsonPath);
+  const packageJson = JSON.parse(templatePackageJsonContents.toString());
+  packageJson.name = packageName;
+  const targetPackageJsonPath = path.join(targetDirectory, 'package.json');
+  await writeFile(targetPackageJsonPath, JSON.stringify(packageJson, null, 2));
+  console.log(`\nDone. To start working with the project run:\n`)
+
+  if (targetDirectory !== cwd) {
+    console.log(`  cd ${path.relative(cwd, targetDirectory)}`)
+  }
+  console.log("  npm install");
+  console.log("  npm run dev");
 }
 
-init().catch(error => {
-  console.error(error);
-})
+try {
+  await main();
+} catch (e) {
+  console.error(e);
+}
